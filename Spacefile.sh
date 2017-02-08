20f8c03d13f045a2876c6d44ca1110fe8677a6f3
Thomas Backlund <blund@blockie.org>
2017-02-08 22:13:24 +0100

    Reupholster SSH function to do multiple jumps

--- Spacefile.sh
+++ Spacefile.sh
@@ -43,45 +43,49 @@ SSH_DEP_INSTALL()
     fi
 }
 
-#================================
+#==================
 # SSH
 #
-# SSH into a remote machine, possibly via a jump host.
+# Connect to remote server over SSH.
+# Optionally use one or more "jump servers"
 #
 # Parameters:
-#   $1: flags
-#   $2: user name
-#   $3: host
-#   $4: port (optional)
-#   $5: keyfile (optional)
-#   $6: command (optional)
+#   $1: host address, or many space separated addresses if using jump hosts.
+#       Last address is the final destination host.
+#   $2: Optional matching list of user names.
+#   $3: Optional matching list of key files.
+#   $4: Optional matching list of ports. e.g. "223 22 222"
+#   $5: Optional matching list of flags. e.g. "q q q"
+#   $6: Optional shell to use on remote side, leave empty for default
+#       login shell. e.g. "sh" or "bash".
+#   $7: Optional command to execute on server, leave blank for interactive shell.
 #
-# Expects:
-#   SSHJUMPHOST: optional - set to use ProxyCommand to connect to the indented host.
-#   SSHJUMPPORT: optional
-#   SSHJUMPUSER: optional
-#   SSHJUMPKEYFILE: optional
+# The parameter lists do not have to be as big as the "hosts" list, if they
+# are not then no or a default value is used.
+# To put on item in the middle of a list as empty use ''.
 #
-#================================
+# Returns:
+#   non-zero on error
+#
+#==================
 SSH()
 {
-    SPACE_SIGNATURE="flags user host [port keyfile shell command]"
-    SPACE_DEP="PRINT"
-    SPACE_ENV="SSHJUMPKEYFILE=${SSHJUMPKEYFILE-} SSHJUMPUSER=${SSHJUMPUSER-} SSHJUMPHOST=${SSHJUMPHOST-}"
+    SPACE_SIGNATURE="host [user keyfile port flags shell command]"
+    SPACE_DEP="PRINT STRING_ESCAPE STRING_ITEM_COUNT STRING_ITEM_GET"
 
-    local sshflags="${1}"
+    local hosts="${1}"
     shift
 
-    local sshuser="${1}"
-    shift
-
-    local sshhost="${1}"
-    shift
-
-    local sshport="${1:-22}"
+    local users="${1-}"
     shift $(( $# > 0 ? 1 : 0 ))
 
-    local sshkeyfile="${1-}"
+    local keyfiles="${1-}"
+    shift $(( $# > 0 ? 1 : 0 ))
+
+    local ports="${1-}"
+    shift $(( $# > 0 ? 1 : 0 ))
+
+    local flagses="${1-}"
     shift $(( $# > 0 ? 1 : 0 ))
 
     local shell="${1-}"
@@ -95,40 +99,71 @@ SSH()
         is_terminal=1
     fi
 
-    if [ "${is_terminal}" = "1" ]; then
-        PRINT "Terminal detected," "debug"
-    else
-        PRINT "Non terminal detected," "debug"
-    fi
+    local count=0
+    STRING_ITEM_COUNT "${hosts}" "count"
 
-    local sshproxycommand=""
-    if [ "${SSHJUMPHOST-}" != "" ]; then
-        sshproxycommand="ProxyCommand=ssh ${is_terminal:+-t} -q ${SSHJUMPKEYFILE:+-i $SSHJUMPKEYFILE} -W %h:%p ${SSHJUMPUSER-$sshuser}@$SSHJUMPHOST -p ${SSHJUMPPORT-22}"
-    fi
+    local sshcommand=""
+    local index=0
+    while [ "${index}" -lt "${count}" ]; do
+        local host=
+        STRING_ITEM_GET "${hosts}" ${index} "host"
 
-    if [ "${SSHJUMPHOST}" = "" ]; then
-        PRINT "Host: ${sshhost}." "info"
-    else
-        PRINT "Host: ${sshhost}, jump host: ${SSHJUMPHOST}." "info"
-    fi
-    # shellcheck disable=2086
-    # shellcheck disable=2029
-    if [ "${shell}" = "" ]; then
-        PRINT "Run in standard shell." "debug"
-        ssh ${sshproxycommand:+-o "${sshproxycommand}"} ${is_terminal:+-t} ${sshflags} ${sshkeyfile:+-i ${sshkeyfile}} ${sshuser:+${sshuser}@}$sshhost -p ${sshport} ${command:+"${command}"}
-    else
-        PRINT "Run in defined shell: ${shell}." "debug"
-        if [ "${command}" != "" ]; then
-            local cm="
-_spaceinvader=\$(cat <<\"_SPACEGAL_SSH_\"
-$command
-_SPACEGAL_SSH_
-)
-bash -c \"\${_spaceinvader}\"
-"
-            ssh ${sshproxycommand:+-o "${sshproxycommand}"} ${is_terminal:+-t} ${sshflags} ${sshkeyfile:+-i ${sshkeyfile}} ${sshuser:+${sshuser}@}$sshhost -p ${sshport} "${cm}"
+        local user=
+        STRING_ITEM_GET "${users}" ${index} "user"
+        if [ "${user}" = "''" ]; then
+            user=
+        fi
+
+        local keyfile=
+        STRING_ITEM_GET "${keyfiles}" ${index} "keyfile"
+        if [ "${keyfile}" = "''" ]; then
+            keyfile=
+        fi
+
+        local port=22
+        STRING_ITEM_GET "${ports}" ${index} "port"
+        if [ "${port}" = "''" ]; then
+            port=22
+        fi
+
+        local flags=
+        STRING_ITEM_GET "${flagses}" ${index} "flags"
+        if [ "${flags}" = "''" ]; then
+            flags=
+        fi
+
+        if [ -z "${sshcommand}" ]; then
+            sshcommand="${keyfile:+-i ${keyfile} }-p ${port} ${flags:+-${flags} }${user:+${user}@}${host}"
         else
-            ssh ${sshproxycommand:+-o "${sshproxycommand}"} ${is_terminal:+-t} ${sshflags} ${sshkeyfile:+-i ${sshkeyfile}} ${sshuser:+${sshuser}@}$sshhost -p ${sshport} "${shell}"
+            STRING_ESCAPE "sshcommand" '"'
+            sshcommand="-o proxycommand=\"ssh -W ${host}:${port} ${sshcommand}\" ${keyfile:+-i ${keyfile} }-p ${port} ${flags:+-${flags} }${user:+${user}@}${host}"
+        fi
+        index=$((index+1))
+    done
+
+    if [ "${is_terminal}" = "1" ]; then
+        sshcommand="ssh -t ${sshcommand}"
+    else
+        sshcommand="ssh ${sshcommand}"
+    fi
+
+    PRINT "${sshcommand}" "debug"
+
+    if [ -z "${shell}" ]; then
+        # No shell given, run in login shell.
+        PRINT "No shell defined, running in default login shell." "debug"
+        STRING_ESCAPE "command" '"$'
+        sh -c "${sshcommand}${command:+ -- \"${command}\"}"
+    else
+        # Run in defined shell.
+        if [ -n "${command}" ]; then
+            PRINT "Run command in defined shell: ${shell}." "debug"
+            STRING_ESCAPE "command" '"$'
+            STRING_ESCAPE "command" '"$'
+            sh -c "${sshcommand} -- ${shell} \"-c \\\"${command}\\\"\""
+        else
+            PRINT "Run defined shell: ${shell}." "debug"
+            sh -c "${sshcommand} -- ${shell}"
         fi
     fi
 }
@@ -141,41 +176,22 @@ bash -c \"\${_spaceinvader}\"
 #
 # Expects:
 #   SSHHOST:
-#   SSHFLAGS: optional
 #   SSHUSER: optional
-#   SSHPORT: optional - defaults to 22.
 #   SSHKEYFILE: optional
+#   SSHPORT: optional - defaults to 22.
+#   SSHFLAGS: optional
 #   SSHSHELL: optional
 #
 # Returns:
-#   0: success
-#   1: missing SSHHOST failure
+#   non-zero on error.
 #
 #================================
 SSH_WRAP()
 {
-    # shellcheck disable=2034
     SPACE_FN="SSH"
-    # shellcheck disable=2153
-    SPACE_BUILDENV="SSHFLAGS=\"${SSHFLAGS-}\" SSHUSER=\"${SSHUSER-}\" SSHHOST=\"${SSHHOST-}\" SSHPORT=\"${SSHPORT-}\" SSHKEYFILE=\"${SSHKEYFILE-}\" SSHSHELL=\"${SSHSHELL-}\" _FORCE_BASH"
-
-    if [ "${_FORCE_BASH}" = "1" ] && [ "${SSHSHELL-}" = "" ]; then
-        SSHSHELL="bash -c"
-    fi
-
-    if [ "${SSHHOST-}" = "" ]; then
-        # This variable is mandatory.
-        _error "SSHHOST variable must be set."
-        return 1
-    fi
-
-    # We evaluate SPACE_ARGS inside this body because
-    # SSHSHELL must have been set first.
-    # shellcheck disable=2034
-    local SPACE_ARGS="\"${SSHFLAGS}\" \"${SSHUSER}\" \"${SSHHOST}\" \"${SSHPORT}\" \"${SSHKEYFILE}\" \"${SSHSHELL}\" \"\${RUN}\""
-    YIELD "SPACE_ARGS"
+    SPACE_ENV="SSHHOST SSHUSER=\"${SSHUSER-}\" SSHKEYFILE=\"${SSHKEYFILE-}\" SSHPORT=\"${SSHPORT-}\" SSHFLAGS=\"${SSHFLAGS-}\" SSHSHELL=\"${SSHSHELL-}\""
+    SPACE_ARGS="\"\${SSHHOST}\" \"\${SSHUSER}\" \"\${SSHKEYFILE}\" \"\${SSHPORT}\" \"\${SSHFLAGS}\" \"\${SSHSHELL}\" \"\${RUN}\""
 }
-
 
 # Disable warning about indirectly checking status code
 # shellcheck disable=SC2181
@@ -249,6 +265,7 @@ SSH_KEYGEN()
 #================================
 SSH_FS()
 {
+    # TODO this needs to be reupholstered just like SSH function.
     SPACE_SIGNATURE="flags user host port keyfile remotepath localpath"
     SPACE_DEP="PRINT FILE_MKDIRP FILE_CHOWN FILE_CHMOD"
     SPACE_ENV="SUDO=${SUDO-} SSHJUMPKEYFILE=${SSHJUMPKEYFILE-} SSHJUMPUSER=${SSHJUMPUSER-} SSHJUMPHOST=${SSHJUMPHOST-}"
@@ -360,8 +377,10 @@ SSH_SSHD_CONFIG()
         return 2
     fi
 
-    row="GatewayPorts yes"
-    FILE_ROW_PERSIST "${row}" "${file}"
+    # This is for port forwarding, which could be used with -g switch, they say.
+    # But is insecure to just allow.
+    #row="GatewayPorts yes"
+    #FILE_ROW_PERSIST "${row}" "${file}"
 }
 
 #===================
